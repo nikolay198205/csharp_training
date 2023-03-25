1a081e2dda4eb47eb9c41a4ece7d46fa0aa31878
Корнев Николай <kornevn@inbox.ru>
2023-03-26 01:14:33 +0700

    Проект создан

---
+++ addressbook-web-test/addressbook-web-test/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace addressbook_web_test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+         
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
