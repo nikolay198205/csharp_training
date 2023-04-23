ee47377704d5e50e146929078a0040c6d0a063b1
Корнев Николай <kornevn@inbox.ru>
2023-04-23 11:32:10 +0700

    Лекция 3.0

---
+++ addressbook-web-test/addressbook-web-test/tests/UniteTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace addressbook_web_test.tests
+{
+
+    internal class UniteTest1
+    {
+        public void TestMetod1()
+
+        {
+            double total = 1000;
+
+            if (total > 1000)
+            {
+                total = total * 0.9;
+                System.Console.Out.Write("Скидка 10%, общая сумма " + total);
+            }
+
+            else
+            {
+                System.Console.Out.Write("Скидка 10%, общая сумма " + total);
+            }
+        }
+
+    }
+}
+
+
+
